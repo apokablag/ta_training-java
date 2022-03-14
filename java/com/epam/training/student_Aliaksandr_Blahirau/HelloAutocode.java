970824486295113f52dda0ccb7336fb239a72e69
apokablag <apokablag@gmail.com>
2022-03-14 22:55:33 +0300

    Initial commit

--- java/com/epam/training/student_Aliaksandr_Blahirau/HelloAutocode.java
+++
@@ -1,7 +0,0 @@
-package com.epam.training.student_Aliaksandr_Blahirau;
-
-public class HelloAutocode {
-    public static void main(String[] args) {
-        System.out.println("Hello, Autocode!");
-    }
-}
