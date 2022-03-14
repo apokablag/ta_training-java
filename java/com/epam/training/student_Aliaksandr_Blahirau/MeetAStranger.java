a04773b88a42d23a31a0bf83d0dec9546d643a02
apokablag <apokablag@gmail.com>
2022-03-14 21:30:12 +0300

    Create gitignore file

---
+++ java/com/epam/training/student_Aliaksandr_Blahirau/MeetAStranger.java
@@ -0,0 +1,11 @@
+package com.epam.training.student_Aliaksandr_Blahirau;
+
+import java.util.Scanner;
+
+public class MeetAStranger {
+    public static void main(String[] args) {
+        Scanner scanOne = new Scanner(System.in);
+        String text = scanOne.nextLine();
+        System.out.println("My text is " + text);
+    }
+}
